3f309f27cd15bb7e933c234fb7b76af9afb60339
Craig Anthony <mcraiganthony@gmail.com>
2015-01-20 14:51:34 -0500

    Craig - hero

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	console.log ('app js loaded');
+
+	// .js-hero__headline reduce opacity when scrolling down
+	$(window).scroll(function(){
+		if($(window).width() > 1260) {
+			windowScroll = $(window).scrollTop();
+			contentOpacity = 1 - (windowScroll / ($('.js-hero--home').offset().top+$('.js-hero--home').height()));
+			$('.js-hero__headline').css('transform','translateY('+Math.floor(windowScroll*0.16)+'px)');
+			$('.js-hero__headline').css('-webkit-transform','translateY('+Math.floor(windowScroll*0.16)+'px)');
+			$('.js-hero__headline').css('opacity',contentOpacity.toFixed(2));
+		}
+	});
+
 });
